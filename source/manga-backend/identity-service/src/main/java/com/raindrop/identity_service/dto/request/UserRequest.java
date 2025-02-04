0ca7b2cb766e9f2f2b0905af2150d1c435825882
DoanQuocDang <doanquocdang39@gmail.com>
2025-02-04 21:16:40 +0700

    identity service: add authentication with jwt

---
+++ source/manga-backend/identity-service/src/main/java/com/raindrop/identity_service/dto/request/UserRequest.java
@@ -0,0 +1,18 @@
+package com.raindrop.identity_service.dto.request;
+
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class UserRequest {
+    @Size(min = 5, message = "USERNAME_INVALID")
+    String username;
+    @Size(min = 8, message = "PASSWORD_INVALID")
+    String password;
+    String email;
+}
---
+++ source/manga-backend/identity-service/src/main/java/com/raindrop/identity_service/entity/User.java
@@ -0,0 +1,24 @@
+package com.raindrop.identity_service.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    String id;
+    String username;
+    String password;
+    String email;
+}
